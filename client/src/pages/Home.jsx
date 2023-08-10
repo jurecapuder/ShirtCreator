98cbc7d1539f08e96b2f72592be615ae4afc3e5f
jurecapuder <capuder.j@gmail.com>
2023-08-10 03:37:49 +0200

    Updating files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        {snap.intro && (
+            <motion.section className='home'{...slideAnimation('left')}>
+                <motion.header>
+                  <img
+                    src='./threejs.png'
+                    alt='logo'
+                    className='w-8 h-8 object-contain' 
+                  />
+                </motion.header>
+            </motion.section>
+        )}
+    </AnimatePresence>
   )
 }
 
