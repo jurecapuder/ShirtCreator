e8a099ed2b97dfc6c390f2c21ca3d13fb8be3fd4
jurecapuder <capuder.j@gmail.com>
2023-08-11 00:23:50 +0200

    Updating files

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture} from '@react-three/drei';
+
+import state from '../store';
 
 const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
   return (
-    <div>Shirt</div>
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+
+        
+      </mesh>
+    </group>
   )
 }
 
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,24 @@
-import { Canvas } from '@react-three/fiber'
-import { Environment, Center } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
+
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CameraRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
   )
 }
 
