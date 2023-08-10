5e1768e020ea55437bce9abfb7745f4d16113739
jurecapuder <capuder.j@gmail.com>
2023-08-11 01:35:58 +0200

    Updating files

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -22,8 +22,26 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
 
-        
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            //map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
       </mesh>
     </group>
   )
