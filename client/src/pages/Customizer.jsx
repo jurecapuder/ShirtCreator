a357d72382bff21b507def2f7201f712f2819a9b
jurecapuder <capuder.j@gmail.com>
2023-08-11 00:04:55 +0200

    Updating files

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -45,6 +45,21 @@ const Customizer = () => {
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
+
+          <motion.div
+            className='filtertabs-container'
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
         </>
       )}
     </AnimatePresence>
