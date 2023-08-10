90726a3a1ce17beed9f4fb6557429e6020f03c14
jurecapuder <capuder.j@gmail.com>
2023-08-11 00:35:49 +0200

    Updating files

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const CameraRig = () => {
-  return (
-    <div>CameraRig</div>
-  )
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    // set the model rotation smoothly
+    easing.dampE(
+    group.current.rotation,
+    [state.pointer.y / 10, -state.pointer.x / 5, 0],
+    0.25,
+    dekta
+    )
+  })
+
+  return <group ref={group}>{children}</group>
 }
 
 export default CameraRig
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -12,12 +12,12 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-      {/* <CameraRig> */}
+      <CameraRig>
         {/* <Backdrop /> */}
         <Center>
           <Shirt />
         </Center>
-      {/* </CameraRig> */}
+      </CameraRig>
     </Canvas>
   )
 }
