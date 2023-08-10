07690c706b0763c2fb38e9cb49b1432b35c2cbd7
jurecapuder <capuder.j@gmail.com>
2023-08-10 04:12:22 +0200

    Updating files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useSnapshot } from 'valtio';
 
 import state from '../store';
 
+import { CustomButton } from '../components';
+
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -16,13 +18,36 @@ const Home = () => {
     <AnimatePresence>
         {snap.intro && (
             <motion.section className='home'{...slideAnimation('left')}>
-                <motion.header>
+                <motion.header {...slideAnimation("down")}>
                   <img
                     src='./threejs.png'
                     alt='logo'
                     className='w-8 h-8 object-contain' 
                   />
                 </motion.header>
+
+                <motion.div className='home-content' {...headContainerAnimation}>
+                    <motion.div {...headTextAnimation}>
+                        <h1 className='head-text'>
+                            LET'S <br className='xl:block hidden' /> DO IT.
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        {...headContentAnimation}
+                        className='flex flex-col gap-5'
+                    >
+                        <p className='max-w-md font-normal text-gray-600 text-base'>
+                            Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{"  "} and define your own style
+                        </p>
+
+                        <CustomButton
+                            type="filled"
+                            title="Customize It"
+                            handleClick={() => state.intro = false}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
+                    </motion.div>
+                </motion.div>
             </motion.section>
         )}
     </AnimatePresence>
