93b345f8708d21f6cfd0757c2bab822f320b8ef6
jurecapuder <capuder.j@gmail.com>
2023-08-11 00:47:04 +0200

    Updating files

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -11,12 +11,28 @@ const CameraRig = ({ children }) => {
   const snap = useSnapshot(state);
 
   useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // set the initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    if(snap.intro) {
+      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if(isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    // set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+
     // set the model rotation smoothly
     easing.dampE(
     group.current.rotation,
     [state.pointer.y / 10, -state.pointer.x / 5, 0],
     0.25,
-    dekta
+    delta
     )
   })
 
