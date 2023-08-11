05b064000594cb400ddd29e71bf09f80661ab290
jurecapuder <capuder.j@gmail.com>
2023-08-12 00:43:44 +0200

    Updating files

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -13,6 +13,33 @@ import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../compone
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab constant depending on the activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker />
+      case "aipicker":
+        return <AIPicker />
+      default:
+        return null;
+    }
+
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -31,6 +58,9 @@ const Customizer = () => {
                     handleClick={() => {}}
                   />
                 ))}
+
+                
+
               </div>
             </div>
           </motion.div>
