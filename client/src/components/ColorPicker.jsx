c89e6e0b12c1573f5c37e65403e1af8d39d61e51
jurecapuder <capuder.j@gmail.com>
2023-08-12 01:02:51 +0200

    Implementing ColorPicker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        presetColors={[
+          "#CCC", "#EFBD4E", "#80C670", "#726DE8",
+          "#353934", "#2CCCE4", "#FF8A65", "#7098DA",
+          "#C19277", "#FF96AD", "#512314", "#5F123D",
+        ]}
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
