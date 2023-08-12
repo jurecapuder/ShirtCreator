f6b9ab8104c985ecf4dcdbc65a9104f14b1c3692
jurecapuder <capuder.j@gmail.com>
2023-08-12 21:29:41 +0200

    Completed DALL.E route

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -18,7 +18,19 @@ router.route('/').get((req, res) => {
 
 router.route('/').post(async (req, res) => {
   try {
-    
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json'
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image })
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong"})
