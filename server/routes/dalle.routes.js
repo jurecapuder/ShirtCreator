d7d330858b3b06a1b1774e1631bba200e5d8c199
jurecapuder <capuder.j@gmail.com>
2023-08-12 21:27:21 +0200

    Adding DALL.E API key

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,8 +6,23 @@ dotenv.config();
 
 const router = express.Router();
 
+const config = new Configuration({
+  apiKey: process.env.OpenAI_API_KEY
+});
+
+const openai = new OpenAIApi(config);
+
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES"})
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong"})
+  }
 })
 
 export default router
