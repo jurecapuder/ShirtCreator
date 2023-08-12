eddaf05433d7d1733283ee03b0287485b4f31bd0
jurecapuder <capuder.j@gmail.com>
2023-08-13 01:15:56 +0200

    Adding download button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -167,7 +167,18 @@ const Customizer = () => {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+
+            {/* Download button */}
+            <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
+
           </motion.div>
+
         </>
       )}
     </AnimatePresence>
